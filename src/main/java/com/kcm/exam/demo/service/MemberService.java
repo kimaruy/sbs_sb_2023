37f0ec3d534bfb5307af60c7f36c3b0d4999f024
kimaruy <https://github.com/kimaruy/sbs_ssg_2023.git>
2023-08-14 13:04:37 +0900

    회원가입 완료 후, 생성된 회원 보여줌

--- src/main/java/com/kcm/exam/demo/service/MemberService.java
+++ src/main/java/com/kcm/exam/demo/service/MemberService.java
@@ -3,6 +3,7 @@ package com.kcm.exam.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.kcm.exam.demo.repository.MemberRepository;
+import com.kcm.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
@@ -11,8 +12,12 @@ public class MemberService {
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
-	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return memberRepository.getLastInsertId();
+	}
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
 	}
 
 }
