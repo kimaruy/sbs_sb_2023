aace4a55530568605fa7a7d7dccee3382f2492f5
kimaruy <https://github.com/kimaruy/sbs_ssg_2023.git>
2023-08-21 10:01:48 +0900

    NeedLoginInterceptor가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/kcm/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kcm/exam/demo/controller/UsrArticleController.java
@@ -15,6 +15,7 @@ import com.kcm.exam.demo.vo.ResultData;
 import com.kcm.exam.demo.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsrArticleController {
@@ -25,10 +26,6 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
 		Rq rq = (Rq) req.getAttribute("rq");
-
-		if (rq.isLogined() == false) {
-			return ResultData.from("F-A", "로그인 후 이용해주세요.");
-		}
 
 		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 작성해주세요.");
